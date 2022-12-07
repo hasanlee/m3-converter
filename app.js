af8a1aca82bd42d7155bff586f726046a9948bce
Hasanli Hasan <hasan@hasanli.info>
2022-12-07 19:11:55 +0400

    some bugs fixed

--- app.js
+++ app.js
@@ -1,40 +1,54 @@
+const currencies = ["AZN", "TRY", "USD", "RUB"];
+const selGroup = document.querySelector(".sell-group");
+const buyGroup = document.querySelector(".buy-group");
 const sell = document.querySelector("#sell");
-const sel = document.querySelector("#sell");
 const buy = document.querySelector("#buy");
-const sellRadio = document.querySelectorAll('input[name="from"]');
-const buyRadio = document.querySelectorAll('input[name="to"]');
+const sellRadio = document.querySelector('input[name="from"]:checked');
+const buyRadio = document.querySelector('input[name="to"]:checked');
+const sellRadios = document.querySelectorAll('input[name="from"]');
+const buyRadios = document.querySelectorAll('input[name="to"]');
+const infoSell = document.querySelector(".info-sell");
+const infoBuy = document.querySelector(".info-buy");
 const API_URL = "https://api.exchangerate.host/latest";
+let fromSymbol;
+let toSymbol;
+let rates;
 
-sel.addEventListener("keyup", convert);
+document.addEventListener(
+  "load",
+  getRatesFromApi(sellRadio.value, buyRadio.value)
+);
+sell.addEventListener("keyup", convert);
 buy.addEventListener("keyup", convert);
-sellRadio.forEach((radio) => {
+sellRadios.forEach((radio) => {
   radio.addEventListener("change", (e) => {
-    console.log(e.target.value);
+    getRatesFromApi(e.target.value, toSymbol);
   });
 });
-buyRadio.forEach((radio) => {
+buyRadios.forEach((radio) => {
   radio.addEventListener("change", (e) => {
-    console.log(e.target.value);
+    getRatesFromApi(fromSymbol, e.target.value);
   });
 });
 
-function getRatesFromApi(){
+function getRatesFromApi(from, to) {
+  fromSymbol = from;
+  toSymbol = to;
 
+  fetch(API_URL + `?base=${fromSymbol}&symbols=${toSymbol}`)
+    .then((res) => res.json())
+    .then((data) => {
+      rates = data.rates;
+      infoSell.innerHTML = `1 ${fromSymbol} = ${rates[toSymbol]} ${toSymbol}`;
+      buy.value = sell.value * rates[toSymbol];
+    });
+  fetch(API_URL + `?base=${toSymbol}&symbols=${fromSymbol}`)
+    .then((res) => res.json())
+    .then((data) => {
+      infoBuy.innerHTML = `1 ${toSymbol} = ${data.rates[fromSymbol]} ${fromSymbol}`;
+    });
 }
 
-
 function convert() {
-  let fromSymbol = "RUB";
-  let toSymbol = "USD";
-  buy.value = sel.value * 5;
-  sellRadio.forEach((s) => {
-    fromSymbol = s.checked ? s.value : "RUB";
-  });
-  buyRadio.forEach((s) => {
-    toSymbol = s.checked ? s.value : "USD";
-  });
-
-  fetch(API_URL+`?base=${fromSymbol}&symbols=${toSymbol}`).then(res=>res.json()).then(data=>console.log(data))
-
-  console.log(fromSymbol, sel.value, toSymbol, buy.value);
+  buy.value = parseFloat(sell.value) * rates[toSymbol];
 }
